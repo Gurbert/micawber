e79d8ac550d4cb62530d180dbabbb65d709b87a9
Gurbert <32823468+Gurbert@users.noreply.github.com>
2019-06-07 17:50:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     author='Charles Leifer',
     author_email='coleifer@gmail.com',
     url='https://github.com/Gurbert/micawber_bs4_classes/',
-      install_requires=['beautifulsoup4',],
+    install_requires=['beautifulsoup4',],
+    download_url = https://github.com/Gurbert/micawber_bs4_classes/archive/0.4.1.tar.gz,
     packages=[p for p in find_packages() if not p.startswith('examples')],
     package_data = {
         'micawber_bs4_classes': [
